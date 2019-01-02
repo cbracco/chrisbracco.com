2330d8a8d1d786bdc39bc7c513f84dd0ab1cf8f7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-02 16:53:18 +0000

    Deploy cbracco/chrisbracco.com to github.com/cbracco/chrisbracco.com.git:gh-pages

--- source/_assets/scripts/modules/detect-js.js
+++
@@ -1,9 +0,0 @@
-/**
- * Replace `.no-js` class with `.js` class on <html> element. Use as a
- * simple method to detect if JavaScript it enabled in the browser.
- */
-
-module.exports = function () {
-  var html = document.documentElement
-  html.className = html.className.replace(/\bno-js\b/g, '') + ' js '
-}
--- source/_work/cardinal-css.md
+++
@@ -1,7 +0,0 @@
----
-layout: work
-title: "Cardinal CSS"
-link: "http://cardinalcss.com"
-previewImage: "/assets/images/work/cardinal-css/thumbnail-widescreen.png"
-order: 3
----
