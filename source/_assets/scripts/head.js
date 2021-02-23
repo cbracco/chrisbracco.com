8c729b30b972799b23225022e572a4768d2eed65
Chris Bracco <chris@cbracco.me>
2021-02-23 16:58:29 -0500

    Remove v1 files

--- source/_assets/scripts/head.js
+++
@@ -1,8 +0,0 @@
-// Define Dependencies
-var detectJS = require('./modules/detect-js')
-
-// Detect if JavaScript is enabled
-detectJS()
-
-// Print success message to console
-console.log('<head> scripts loaded.')
