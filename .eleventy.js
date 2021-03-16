ad290bb02deb0a140153f68a9998d83162e40398
Chris Bracco <chris@cbracco.me>
2021-03-15 21:36:18 -0400

    Pass CNAME file through to production bundle

--- .eleventy.js
+++ .eleventy.js
@@ -73,6 +73,7 @@ module.exports = function (config) {
     config.addPassthroughCopy({ 'src/assets/images': 'assets/images' });
     config.addPassthroughCopy({ 'src/assets/audio': 'assets/audio' });
     config.addPassthroughCopy('*.txt');
+    config.addPassthroughCopy('CNAME');
 
     // Set Markdown libraries
     config.setLibrary('md', markdownIt({
